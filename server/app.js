979b5d5928f4e35aa8e2cf6080f3158a9f478c6b
bogdan pryvalov <pryvalov.bogddan@gmail.com>
2021-11-06 21:16:39 +0200

    added client react

--- server/app.js
+++ server/app.js
@@ -3,14 +3,18 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const { AuthStringToMongo } = require('./pass');
+const cors = require('cors');
 
 const app = express();
 
-// to connect to mongodb with https://cloud.mongodb.com/
+// to connect to mongodb with https://cloud.mongodb.com/ mlab
 mongoose.connect(`mongodb+srv://${AuthStringToMongo}@cluster0.o2rlb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 mongoose.connection.once('open', () => {
 	console.log('connected to database')
 });
+
+// to remove block by CORS policy
+app.use(cors());
 
 // to react for all requests with graphql
 app.use('/graphql', graphqlHTTP({
